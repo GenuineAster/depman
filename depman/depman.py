846f075ffcaf2efc691add33cdfda5cc1b56a4aa
Mischa Aster Alff <aster@is.a.cxx.ninja>
2018-08-09 15:58:03 +0200

    Add copyright notice and license to depman.py

--- depman/depman.py
+++ depman/depman.py
@@ -3,6 +3,8 @@
 """
 depman - Dependency management for those who are tired of dependency management
 
+depman is licensed under the MIT license, and is (c) 2018 Mischa Alff. See the bottom of this file for the full license
+
 depman is a source dependency management tool that aims to be as simple and functional as possible.
 
 See https://github.com/Mischa-Alff/depman for more information.
@@ -217,3 +219,30 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
+"""
+License:
+
+MIT License
+
+Copyright (c) 2018 Mischa Aster Alff
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
